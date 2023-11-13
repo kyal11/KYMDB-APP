6020d1089fbfef68a3c29c920f51a2b7558ddee9
Rizky Al Arief <rizkyalarief11@gmail.com>
2023-11-13 18:38:35 +0700

    Fix : Recycle View List

---
+++ app/src/main/java/com/example/kymdb/ListMovieAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.kymdb
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class ListMovieAdapter(private val listMovie : ArrayList<Movies>) : RecyclerView.Adapter<ListMovieAdapter.ListViewHolder>() {
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvTitle: TextView = itemView.findViewById(R.id.tv_title)
+        val tvYear: TextView = itemView.findViewById(R.id.tv_releasedate)
+        val tvGenre: TextView = itemView.findViewById(R.id.tv_genre)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_description)
+        val imgPoster: ImageView = itemView.findViewById(R.id.iv_poster)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_movies, parent, false)
+        return  ListViewHolder(view)
+    }
+
+    override fun getItemCount() : Int = listMovie.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+       val (title, year, genre, description, poster) = listMovie[position]
+        holder.tvTitle.text = title
+        holder.tvYear.text = year.toString()
+        holder.tvGenre.text = genre
+        holder.tvDescription.text = description
+        Glide.with(holder.itemView.context)     //Insert Poster
+            .load(poster)
+            .into(holder.imgPoster)
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/kymdb/MainActivity.kt
+++ app/src/main/java/com/example/kymdb/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.kymdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvMovies: RecyclerView
+    private val list = ArrayList<Movies>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvMovies = findViewById(R.id.rv_movies)
+        rvMovies.setHasFixedSize(true)
+
+        list.addAll(getListMovies())
+        showRecyclerList()
+    }
+
+    private fun getListMovies(): ArrayList<Movies> {
+        val dataTitle = resources.getStringArray(R.array.data_movie)
+        val dataYear = resources.getIntArray(R.array.data_release)
+        val dataGenre = resources.getStringArray(R.array.data_genre)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPoster = resources.getStringArray(R.array.data_poster)
+        val listMovies = ArrayList<Movies>()
+
+        for (i in dataTitle.indices) {
+            val movies = Movies(dataTitle[i], dataYear[i], dataGenre[i], dataDescription[i], dataPoster[i])
+            listMovies.add(movies)
+        }
+
+        return listMovies
+    }
+
+    private fun showRecyclerList() {
+        rvMovies.layoutManager = LinearLayoutManager(this)
+        val listMovieAdapter = ListMovieAdapter(list)
+        rvMovies.adapter = listMovieAdapter
     }
 }
